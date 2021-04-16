974f0d2928a85f9a8b3fc27e3fb823fe4cb9541d
Cyndexz <cynthiahernandez1108@hotmail.com>
2021-04-16 01:09:21 -0500

    Add picture buttons * Add pictures as buttons * Add text underneath to lead to the page * Center dashboard text * Show accumulated comissions

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+                $commission = DB::table('users')->where('id', Auth::user()->id)->first()->accumulated_commission;
+                return view('home', ['commission' => $commission]);
     }
 }
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center"><h4>{{ __('Dashboard') }}</h4></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,42 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <!-- Big icons -->
+                    <div class="d-flex flex-wrap p-1 mx-auto">
+                        <div class="p-5">
+                            <a href="/quotes" alt="New Quotes" title="New Quotes" class="text-dark text-decoration-none">
+                                <img src="/img/chat.png" height=128 width=128 alt="New Quote">
+                                <p class="text-center font-weight-bold">Quotes</p>
+                            </a>
+                        </div>
+                        <div class="p-5">
+                            <a href="/customers" alt="Customers" title="Customers" class="text-dark text-decoration-none">
+                                <img src="/img/notepad.png" height=128 width=128 alt="Customers">
+                                <p class="text-center font-weight-bold">Customers</p>
+                            </a>
+                        </div>
+                        <div class="p-5">
+                            <a href="/users" alt="Users" title="Users" class="text-dark text-decoration-none">
+                                <img src="/img/team.png" height=128 width=128 alt="Users">
+                                <p class="text-center font-weight-bold">Users</p>
+                            </a>
+                        </div>
+                        <div class="p-5">
+                            <a href="/orders" alt="Orders" title="Orders" class="text-dark text-decoration-none">
+                                <img src="/img/folder.png" height=128 width=128 alt="Orders">
+                                <p class="text-center font-weight-bold">Orders</p>
+                            </a>
+                        </div>
+                        <div class="p-5">
+                            <a href="/users" alt="Users" title="Users" class="text-dark text-decoration-none">
+                                <img src="/img/coins.png" height=128 width=128 alt="Uhh idk">
+                                <p class="text-center font-weight-bold">?</p>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- User's commissions -->
+                    <h4 class="text-center"><span style="color: #222a80;">Your accumulated commissions:</span> <span style="color: #127522;">${{$commission}}</span></h4>
+                </div>
                 </div>
             </div>
         </div>
