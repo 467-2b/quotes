b6d79b558e00e034dd3ae4d9b9d87c7c2456da4b
zgjs <z1871157@students.niu.edu>
2021-04-18 00:33:46 -0500

    Add RoleAndPermissionSeeder

---
+++ laravel/database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::where('username', 'clerk')->first()->assignrole('clerk');
+        User::where('username', 'sales')->first()->assignrole('sales');
+    }
+}
