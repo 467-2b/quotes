9593a0587f8b3f01c4bf37c99c4d5a1c4e0524e0
zgjs <z1871157@students.niu.edu>
2021-04-19 01:58:59 -0500

    Add ability to create new quotes

--- laravel/app/Http/Controllers/QuoteController.php
+++ laravel/app/Http/Controllers/QuoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quote;
+use Illuminate\Support\Facades\Validator;
 
 class QuoteController extends Controller
 {
@@ -17,14 +18,87 @@ class QuoteController extends Controller
         $this->middleware('auth');
     }
 
+
+    /**
+     * Get a validator for an incoming newquote request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'associate_id' => ['required', 'int', 'exists:users,id'],
+            'customer_id' => ['required', 'int', 'exists:legacydb.customers,id'],
+            'customer_email' => ['required', 'string', 'email', 'max:255'],
+        ]);
+    }
+
+    /**
+     * Create a new quote instance after a valid newquote submission.
+     *
+     * @param  array  $data
+     * @return \App\Models\Quote
+     */
+    protected function create(array $data)
+    {
+        $customer_name = \App\Models\Customer::find($data['customer_id'])->name;
+        return Quote::create([
+            'associate_id' => $data['associate_id'],
+            'customer_id' => $data['customer_id'],
+            'customer_email' => $data['customer_email'],
+            'customer_name' => $customer_name
+        ]);
+    }
+
+    /**
+     * Handle a newquote request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $quote = $this->create($request->all());
+        return redirect(route('quote', $quote->id));
+    }
+
+    /**
+     * Show the form to create a new quote
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function new()
+    {
+        $customers = \App\Models\Customer::all();
+        return view('newquote', compact('customers'));
+    }
+
     /**
      * Show the quotes list
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function quotes()
+    public function index()
     {
         $quotes = Quote::all();
-        return view('quotes', ['quotes' => $quotes]);
-    }   
+        return view('quotes', compact('quotes'));
+    }
+
+    /**
+     * Show the quote info and edit forms
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function quote($id)
+    {
+        $customers = \App\Models\Customer::all();
+        $quote = \App\Models\Quote::find($id);
+        $line_items = $quote->line_items;
+        $notes = $quote->notes->where('secret', false);
+        $secret_notes = $quote->notes->where('secret', true);
+        return view('quote', compact('customers', 'quote', 'line_items', 'notes', 'secret_notes'));
+    }
 }
