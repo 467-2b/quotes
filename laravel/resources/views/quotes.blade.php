294c4257d051b543ff8146f2824c51a2e88e5c8e
maxwentmissing <maxwentmissing@gmail.com>
2021-04-22 06:38:21 -0500

    Make quotes table filterable and sortable and searchable. Add line item function still off

--- laravel/resources/views/quotes.blade.php
+++ laravel/resources/views/quotes.blade.php
@@ -8,7 +8,15 @@
             <div class="card">
                 <div class="card-header text-center"><h4>{{ __('Quotes') }}</h4></div>
                 <div class="card-body">
-                    <table class="table table-striped ">
+                <div>
+                <select id="statusFilter" class = "form-control" style="width:150px;">
+                    <option value="">Any status</option>
+                    <option value="unfinalized">unfinalized</option>
+                    <option value="finalized">finalized</option>
+                    <option value="sanctioned">sanctioned</option>
+                </select>
+                </div>
+                    <table class="table table-striped " id="quotesTable">
                         <thead class="thead-dark">
                             <tr>
                                 <th scope="col">Customer</th>
@@ -43,3 +51,41 @@
 </div>
 
 @endsection
+
+@push('script')
+    <script src="//code.jquery.com/jquery-1.12.3.js" ></script>
+    <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js" defer></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" defer/>
+    <script>
+    $(document).ready(function() {
+        $('#quotesTable').DataTable({
+            "searching":true
+        });
+        var table = $('#quotesTable').DataTable();
+        $("#quotesTable_filter.dataTables_filter").append($("#statusFilter"));
+        var categoryIndex = 0;
+      $("#quotesTable th").each(function (i) 
+      {
+        if ($($(this)).html() == "Status") 
+        {
+          statusIndex = i; 
+          return false;
+        }
+      });
+      $.fn.dataTable.ext.search.push(
+        function (settings, data, dataIndex) {
+          var selected = $('#statusFilter').val()
+          var status = data[statusIndex];
+          if (selected === "" || status === selected) {
+            return true;
+          }
+          return false;
+        }
+      );
+      $("#statusFilter").change(function (e) {
+        table.draw();
+      });
+      table.draw();
+    });
+    </script>
+@endpush
