ea2535c3b4c667479ef376d0a6224a2e12bd644e
maxwentmissing <maxwentmissing@gmail.com>
2021-04-22 13:41:27 -0400

    Make quotes table filterable by status and filterable.

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    @stack('script')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
